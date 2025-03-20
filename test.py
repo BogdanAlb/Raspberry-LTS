e560a18f31c12c3e5077d1cc2353582600e5dc6b
Bogdan Alb <142724023+BogdanAlb@users.noreply.github.com>
2025-03-20 17:07:45 +0100

    primul test

---
+++ test.py
@@ -0,0 +1,11 @@
+import hx711 from HX711
+
+hx = HX711(dout=5, pd_sck=6)
+hx.set_reading_format("MSB", "MSB")
+hx.set_reference_unit(92)
+hx.reset()
+hx.tare()       
+print(hx.get_weight(5))
+hx.power_down()
+hx.power_up()
+
